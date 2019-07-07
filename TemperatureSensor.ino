ccde0b7a8f38eb589c7f1d66aff0778b67535fee
Naty98 <50978073+Naty98@users.noreply.github.com>
2019-07-06 23:08:14 -0500

    Add files via upload

---
+++ TemperatureSensor.ino
@@ -0,0 +1,25 @@
+//LM35////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Declaracion de variables globales
+      float tempC; // Variable para almacenar el valor obtenido del sensor (0 a 1023)
+      int pinLM35 = 0; // Variable del pin de entrada del sensor (A0)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+        Serial.begin(9600);
+        analogReference(INTERNAL); 
+}
+
+void loop() {
+    //Sensor LM35 (Sensor de Temperatura)
+          // Con analogRead (Valores de 0 a 1023)
+          tempC = analogRead(pinLM35); 
+             
+            // Calculamos la temperatura con la fórmula
+            tempC = (1.1 * tempC * 100.0)/1024.0; 
+          
+            // Envia el dato al puerto serial
+            Serial.print(tempC);
+            Serial.println(" °C");
+            
+            // Esperamos un tiempo para repetir el loop
+            delay(1000);
+}
